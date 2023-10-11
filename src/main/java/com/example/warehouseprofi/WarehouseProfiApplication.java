fe611f29f3cc9efb7dac35e37d6c34f5986fee9a
nikitaalekseev <nikita-alekseev2406@mail.ru>
2023-10-11 17:29:14 +0300

    Added initial files and modified pom.xml

---
+++ src/main/java/com/example/warehouseprofi/WarehouseProfiApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouseprofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseProfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseProfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warehouseprofi/WarehouseProfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouseprofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseProfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
